f38cf410e04b374d74bb28a6d1bddc92d92136dd
Astafurov-mb <79689882+Astafurov-mb@users.noreply.github.com>
2023-02-21 18:17:20 +0400

    done

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -14,45 +14,29 @@
 """
 import logging
 import ephem
+import Config
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
-logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO,
-                    filename='bot.log')
-
-
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
-    }
-}
-
-
-def greet_user(update, context):
-    text = 'Вызван /start'
-    print(text)
-    update.message.reply_text(text)
-
+def say_hello(update, context):
+    update.message.reply_text ('Здарова!')
+    print(update)
+    print(context)
+    update.message.first_name
 
 def talk_to_me(update, context):
     user_text = update.message.text
     print(user_text)
-    update.message.reply_text(text)
+    update.message.reply_text('ДАННОЕ СООБЩЕНИЕ (МАТЕРИАЛ) СОЗДАНО И (ИЛИ) РАСПРОСТРАНЕНО ИНОСТРАННЫМ СРЕДСТВОМ МАССОВОЙ ИНФОРМАЦИИ, ВЫПОЛНЯЮЩИМ ФУНКЦИИ ИНОСТРАННОГО АГЕНТА, И (ИЛИ) РОССИЙСКИМ ЮРИДИЧЕСКИМ ЛИЦОМ, ВЫПОЛНЯЮЩИМ ФУНКЦИИ ИНОСТРАННОГО АГЕНТА \n\n\n' + user_text)
 
+logging.basicConfig(filename='bot.log', level=logging.INFO)
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
-
-    dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
+    mybot = Updater(Config.API_KEY, use_context=True)
+    dp=mybot.dispatcher
+    dp.add_handler(CommandHandler('start', say_hello))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-
+    logging.info('стартуем!')
     mybot.start_polling()
     mybot.idle()
 
-
-if __name__ == "__main__":
-    main()
+main()
